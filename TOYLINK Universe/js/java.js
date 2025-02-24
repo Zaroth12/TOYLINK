6583236af916b8a133db9856b2e079ed73528b89
Zaroth12 <mestepa@unicolmayor.edu.co>
2025-02-24 18:28:13 -0500

    Add files via upload

---
+++ TOYLINK Universe/js/java.js
@@ -0,0 +1,16 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100
+        },
+        "size": {
+            "value": 2
+        },
+        "move": {
+            "speed": 1
+        },
+        "line_linked": {
+            "enable": false
+        }
+    }
+});
